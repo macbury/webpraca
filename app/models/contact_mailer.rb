456015cb31567716c6efa2a588bdc9e8cfa3cfeb
macbury <macbury@gmail.com>
2010-01-05 00:02:17 +0100

    NewRelic gem

--- app/models/contact_mailer.rb
+++ app/models/contact_mailer.rb
@@ -5,7 +5,7 @@ class ContactMailer < ActionMailer::Base
     
     @recipients  = contact_handler.job.nil? ? ExceptionNotifier.exception_recipients : contact_handler.job.email
     @from        = contact_handler.email
-    @subject     = "Kontakt od: #{contact_handler.subject}"
+    @subject     = "Kontakt od: #{contact_handler.email}"
     
     @body[:contact_handler] = contact_handler
     
--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -2,6 +2,7 @@ require "smtp_tls"
 ActionMailer::Base.delivery_method = :smtp   
 ActionMailer::Base.default_charset = 'utf-8'
 ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true  
 ActionMailer::Base.smtp_settings = YAML.load_file("#{RAILS_ROOT}/config/mailer.yml")[Rails.env]
 ActionMailer::Base.smtp_settings[:tsl] = true
 ActionMailer::Base.smtp_settings[:authentication] = :login
