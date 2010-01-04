3b4694766faed599313c7ff71ac761a5a4a7edda
macbury <macbury@gmail.com>
2010-01-04 23:56:51 +0100

    rails env

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-set :environment, Rails.env
+set :environment, "production"
 every :hour do # Many shortcuts available: :hour, :day, :month, :year, :reboot
  	rake "validates_captcha:create_static_images COUNT=50"
 end
