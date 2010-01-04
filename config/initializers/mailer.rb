b7b3eb342df8958439eb0649c8cdaa23940895a2
macbury <macbury@gmail.com>
2010-01-04 23:34:11 +0100

    Mailer megiteam

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -3,3 +3,5 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.default_charset = 'utf-8'
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.smtp_settings = YAML.load_file("#{RAILS_ROOT}/config/mailer.yml")[Rails.env]
+ActionMailer::Base.smtp_settings[:tsl] = true
+ActionMailer::Base.smtp_settings[:authentication] = :login
