d1c7c29166a073db613fb1c723e7f1fd4ba07d78
macbury <macbury@gmail.com>
2010-01-06 14:22:41 +0100

    cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,8 @@
 set :environment, "production"
 set :output, '/home/webpraca/www/apps/webpraca/shared/log/cron.log'
+set :path, '/home/webpraca/www/apps/webpraca/current/'
 
-every 1.hour do # Many shortcuts available: :hour, :day, :month, :year, :reboot
+every 1.minute do # Many shortcuts available: :hour, :day, :month, :year, :reboot
  	rake "validates_captcha:create_static_images COUNT=50"
 end
 
