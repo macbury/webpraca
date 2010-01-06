cd22b6f62729a399c994ec6326c46d0c7bfc5154
macbury <macbury@gmail.com>
2010-01-06 15:02:27 +0100

    Gem path

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,7 @@
 set :environment, "production"
 set :output, '/home/webpraca/www/apps/webpraca/shared/log/cron.log'
-set :GEM_PATH, '/home/webpraca/www/.ruby/gems/1.8:/var/lib/gems/1.8/'
+env, :GEM_PATH, '/home/webpraca/www/.ruby/gems/1.8:/var/lib/gems/1.8/'
+
 every 1.minute do # Many shortcuts available: :hour, :day, :month, :year, :reboot
  	rake "validates_captcha:create_static_images COUNT=50"
 end
