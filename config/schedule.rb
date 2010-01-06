28b6bb71e0a6dcd9ec2a51c3749670edd47fd174
macbury <macbury@gmail.com>
2010-01-06 18:23:27 +0100

    Captchas

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,8 +2,9 @@ set :environment, "production"
 set :output, '/home/webpraca/www/apps/webpraca/shared/log/cron.log'
 env :GEM_PATH, '/home/webpraca/www/.ruby/gems/1.8:/var/lib/gems/1.8/'
 
-every 1.hour do # Many shortcuts available: :hour, :day, :month, :year, :reboot
- 	rake "validates_captcha:create_static_images COUNT=50"
+every 1.day, :at => '1:00 am' do 
+ 	rake "validates_captcha:create_static_images COUNT=500"
+	command "restart-app webpraca"
 end
 
 every 1.day, :at => '1:30 am' do
